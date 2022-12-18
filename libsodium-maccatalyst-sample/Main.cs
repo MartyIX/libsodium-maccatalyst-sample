d7644bbfe682f2484cee2dcb73d9c0b7c1976f81
MartyIX <203266+MartyIX@users.noreply.github.com>
2022-12-18 20:33:50 +0100

    Initial commit

---
+++ libsodium-maccatalyst-sample/Main.cs
@@ -0,0 +1,7 @@
+﻿using libsodium_maccatalyst_sample;
+
+// This is the main entry point of the application.
+// If you want to use a different Application Delegate class from "AppDelegate"
+// you can specify it here.
+UIApplication.Main (args, null, typeof (AppDelegate));
+
